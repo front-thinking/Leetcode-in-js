9690fcfaf62469f572ee979bf929d88c2188a511
Yujia Luo <yarmyarch@live.cn>
2014-01-01 23:33:47 -0800

    Someissues updated.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Leetcode-in-js
 ==============
 
-一些个算法练习。题目来自http://oj.leetcode.com/problems/，和一些其他偶尔看见的问题，Leetcode亦并非全部解答。
+一些个js的算法练习。
+题目来自http://oj.leetcode.com/problems/<br>
+和一些其他偶尔看见的问题，Leetcode亦并非全部解答。
--- creater.js
+++ creater.js
@@ -1,3 +1,5 @@
+// Just some code used to create a random array with given size.
+// Would be used for some test cases such as sort.
 var creat = function(n) {
     var rlt = [];
     for (var i = 0; i < n; ++i) {
