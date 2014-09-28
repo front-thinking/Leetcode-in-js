7ce420d5de37e5c04bb1cd950fe8d9ed9a1ad5e1
Yujia Luo <yarmyarch@live.cn>
2014-09-29 01:03:51 +0800

    longestSubstring, simple and easy.

---
+++ longestSubstring.js
@@ -0,0 +1,27 @@
+//@ sourceURL=longestSubstring.js
+/**
+ * @see https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
+ * 
+Given a string, find the length of the longest substring without repeating characters. 
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ *
+ */
+var longestSubstring = function(str) {
+    var longestRecord = "",
+        curChar,
+        curRecord = "";
+    
+    for (var i = 0, len = str.length; i < len; ++i) {
+        curChar = str.charAt(i);
+        if (!curRecord.match(curChar)) {
+            curRecord += curChar;
+        } else {
+            curRecord = curRecord.replace(new RegExp("^.*?" + curChar), "");
+        }
+        if (longestRecord.length <= curRecord.length) {
+            longestRecord = curRecord;
+        }
+    }
+    
+    return longestRecord;
+}
