86c2a95654e68f7ee055dac3527d91923808cc91
Yujia Luo <yarmyarch@live.cn>
2014-09-30 17:01:32 +0800

    advanced version of inherited

--- inherite.js
+++ inherite.js
@@ -44,3 +44,84 @@ Dog.prototype.constructor = Dog;
 var puppy = new Dog();
 puppy.run();
 puppy._super.run();
+
+// Advanced version
+var extend = function(superClass, subClass) {
+    
+    var newChild = function() {
+        var _super = {};
+        
+        superClass.apply(this, arguments);
+        arguments.callee.prototype = this;
+        for (var i in this) {
+            Object.defineProperty(_super, i, {
+                writable : false,
+                value : this[i]
+            });
+            //~ _super[i] = this[i];
+        }
+        Object.defineProperty(this, "_super", {
+            get : function() {
+                if (arguments.callee.caller == subClass) {
+                    return _super;
+                } else {
+                    return undefined;
+                }
+            },
+            set : function() {
+                return false;
+            },
+            configurable : true
+        });
+        //~ this._super = _super;
+        subClass.apply(this, arguments);
+    }
+    newChild.prototype = new superClass();
+    if (!newChild.prototype instanceof superClass) {
+        // no non-param constructor in super class.
+        //~ return false;
+    }
+    return newChild;
+}
+
+var Animal = function(){
+    
+    // private
+    var leg = "animal";
+    
+    // public
+    this.run = function() {
+        console.log(leg);
+    };
+};
+
+var Dog = extend(Animal, function() {
+    var leg = "dog",
+        _super = this._super;
+    
+    this.run = function() {
+        _super && _super.run();
+        console.log(leg);
+    }
+});
+
+// puppy instanceof Dog;
+// puppy instanceof Animal.
+var puppy = new Dog();
+puppy.run();
+
+var Pig = extend(Dog, function() {
+    var leg = "pig";
+        _super = this._super;
+    
+    this.run = function() {
+        _super.run();
+        console.log(leg);
+    }
+});
+
+// piggy instanceof Pog;
+// piggy instanceof Dog;
+// piggy instanceof Animal.
+var piggy = new Pig();
+piggy.run();
